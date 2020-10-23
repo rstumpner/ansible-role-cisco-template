051e7596ed1b463e8ee0065ab453b3e1a83296e6
Roland Stumpner <roland@stumpner.at>
2020-10-23 15:21:17 +0200

    changed ansible-role-cisco-template add template interfaces

--- tests/ciscoconfparse/cisco-cli-ut.py
+++ tests/ciscoconfparse/cisco-cli-ut.py
@@ -5,3 +5,6 @@ parse = CiscoConfParse(sys.argv[1], syntax='ios')
 
 for intf_obj in parse.find_objects('^hostname'):
     print("hostname: " + intf_obj.text)
+
+for intf_obj in parse.find_objects('^GigabitEthernet'):
+    print("Interfaces: " + intf_obj.text)
