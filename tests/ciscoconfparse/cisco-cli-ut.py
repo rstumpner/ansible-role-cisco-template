e84f786fb903731a6efb4e2af04264f155f04545
Roland Stumpner <roland@stumpner.at>
2020-10-24 20:14:38 +0200

    changed repo update unit test ciscoconfparse

--- tests/ciscoconfparse/cisco-cli-ut.py
+++ tests/ciscoconfparse/cisco-cli-ut.py
@@ -4,7 +4,7 @@ from ciscoconfparse import CiscoConfParse
 parse = CiscoConfParse(sys.argv[1], syntax='ios')
 
 for hostname_obj in parse.find_objects('^hostname'):
-    print("hostname: " + intf_obj.text)
+    print("hostname: " + hostname_obj.text)
 
 for intf_obj in parse.find_objects('^interface'):
     print("Interfaces: " + intf_obj.text + "\n" )
