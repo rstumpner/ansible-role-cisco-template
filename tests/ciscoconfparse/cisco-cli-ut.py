137f4e8a3acbb2f56ce6e945b7f3fd8bdaaae6cc
Roland Stumpner <roland@stumpner.at>
2020-10-26 18:29:20 +0100

    changed cicd stage testing unit test ciscoconfparse fix

--- tests/ciscoconfparse/cisco-cli-ut.py
+++ tests/ciscoconfparse/cisco-cli-ut.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python
 
 import sys
+from sys import argv
 import argparse
 from ciscoconfparse import CiscoConfParse
 
 
-def test_hostname():
-    global tests_total
+def testhostname():
+    tests = 0
     for hostname_obj in parse.find_objects('^hostname'):
         print("Found hostname: " + hostname_obj.text)
-        test_total += 1
-    return;
+        tests = tests+1
+    return tests  
 
-def test_interfaces():
-    global tests_total
+def testinterfaces():
+    tests = 0
     for intf_obj in parse.find_objects('^interface'):
         print("Interfaces: " + intf_obj.text + "\n" )
-        tests_total += 1
-    return;
+        tests += 1
+    return tests
 
 def start():
     script, input_file = argv
     global parse
-    global tests_total
-
+    # Lesen der Cisco Config Datei
     parse = CiscoConfParse(input_file, syntax='ios')
-
-    test_hostname()
-    test_interfaces()
-    print("TOTAL" + tests_total + "\n")
-    return;
+    # Auswerten der Tests für einen Report
+    teststotal = 0
+    teststotal = teststotal + testhostname()
+    teststotal = teststotal + testinterfaces()
+#    print(teststotal)
+    print("TOTAL:" + str(teststotal) + "\n")
+    
 
 def main():
     args = sys.argv[1:]
     if len(args) == 1:
         start()
-    return;
+    else:
+        print("Please give me a Filename as input")
 
 if __name__ == "__main__":
     main()
