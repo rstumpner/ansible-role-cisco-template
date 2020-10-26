20a6f3ea854e394f95b49be06b446c7943e10be4
Roland Stumpner <roland@stumpner.at>
2020-10-26 16:19:53 +0100

    changed cicd stage test unit test with ciscoconfparse

--- tests/ciscoconfparse/cisco-cli-ut.py
+++ tests/ciscoconfparse/cisco-cli-ut.py
@@ -1,10 +1,33 @@
+#!/usr/bin/env python
+
 import sys
+import argparse
 from ciscoconfparse import CiscoConfParse
 
-parse = CiscoConfParse(sys.argv[1], syntax='ios')
 
-for hostname_obj in parse.find_objects('^hostname'):
-    print("hostname: " + hostname_obj.text)
+def test_hostname()
+    for hostname_obj in parse.find_objects('^hostname'):
+        print("hostname: " + hostname_obj.text)
 
-for intf_obj in parse.find_objects('^interface'):
-    print("Interfaces: " + intf_obj.text + "\n" )
+def test_interfaces()
+    for intf_obj in parse.find_objects('^interface'):
+        print("Interfaces: " + intf_obj.text + "\n" )
+
+def start():
+script, input_file = argv
+global parse
+global test_total
+
+parse = CiscoConfParse(input_file, syntax='ios')
+
+test_hostname()
+test_interfaces()
+
+
+def main():
+args = sys.argv[1:]
+if len(args) == 1:
+    start()
+
+if __name__ = "__main__":
+main()
