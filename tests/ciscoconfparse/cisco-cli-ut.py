0c8439fa4b7923fcf8fb0fc54bfd179f449a68ca
Roland Stumpner <roland@stumpner.at>
2020-10-26 16:57:42 +0100

    changed cicd stage test unit testing ciscoconfparse fix

--- tests/ciscoconfparse/cisco-cli-ut.py
+++ tests/ciscoconfparse/cisco-cli-ut.py
@@ -6,18 +6,18 @@ from ciscoconfparse import CiscoConfParse
 
 
 def test_hostname():
-global tests_total
+    global tests_total
     for hostname_obj in parse.find_objects('^hostname'):
         print("Found hostname: " + hostname_obj.text)
         test_total += 1
-return;
+    return;
 
 def test_interfaces():
-global tests_total
+    global tests_total
     for intf_obj in parse.find_objects('^interface'):
         print("Interfaces: " + intf_obj.text + "\n" )
         tests_total += 1
-return;
+    return;
 
 def start():
     script, input_file = argv
@@ -29,14 +29,13 @@ def start():
     test_hostname()
     test_interfaces()
     print("TOTAL" + tests_total "\n")
-
-return;
+    return;
 
 def main():
     args = sys.argv[1:]
     if len(args) == 1:
         start()
-return;
+    return;
 
 if __name__ = "__main__":
 main()
